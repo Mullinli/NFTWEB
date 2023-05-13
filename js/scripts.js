4fbe4417faede043af6bda5a44ee94c2bd0ac91b
Yaroslav <70465614+Mullinli@users.noreply.github.com>
2023-05-13 11:56:31 +0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -59,5 +59,12 @@ $(function () {
             scrollTop: 0
         }, 1000);
     });
+
+    var BotStar={appId:"s5e30b070-f16a-11ed-8c7b-45d7dd970431",mode:"livechat"};
+    !function(t,a){var e=function(){(e.q=e.q||[]).push(arguments)};
+    e.q=e.q||[],t.BotStarApi=e;!function(){var t=a.createElement("script");
+    t.type="text/javascript",t.async=1,t.src="https://widget.botstar.com/static/js/widget.js";
+    var e=a.getElementsByTagName("script")[0];e.parentNode.insertBefore(t,e)}();}
+    (window,document)
     
 });
